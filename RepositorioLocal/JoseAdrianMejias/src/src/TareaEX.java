760107e3e3d727a9f19dcf52c00180433105c29f
Adrivink <94827470+Adrivink@users.noreply.github.com>
2021-12-13 09:37:36 +0100

    Add files via upload

---
+++ RepositorioLocal/JoseAdrianMejias/src/src/TareaEX.java
@@ -0,0 +1,18 @@
+package src;
+import java.util.Scanner;
+public class TareaEX {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner reader = new Scanner(System.in);
+		System.out.println("Introduzca un número");
+		int numero = reader.nextInt();
+		if(numero%2==0) {
+		System.out.println("El número es par");
+		} else {
+		System.out.println("El número es impar");
+		}
+		reader.close();
+	}
+
+}
